a40f41f5ae4625f4333b86142ca804a22a1d4af5
Henok-Tekeba <tekebahenok6@gmail.com>
2026-06-26 21:04:29 +0300

    remove about section from homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,6 @@ import NavBar from '../components/NavBar'
 import useReveal from '../hooks/useReveal'
 import Hero from '../components/Hero'
 import GitHubCommitGraph from '../components/GitHubCommitGraph'
-import About from '../components/About'
 import Projects from '../components/Projects'
 import Skills from '../components/Skills'
 import Contact from '../components/Contact'
@@ -19,7 +18,6 @@ export default function HomePage() {
       <NavBar />
       <Hero />
       <GitHubCommitGraph />
-      <About />
       <Projects />
 
       <Skills />
